bd86d1945b3e98867345ca33ce49cfe89dcfe0f7
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-11-19 20:29:01 +0100

    Shell script to test pushand works as expected

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SERVER='fesplugas@labs.francescesplugas.com'
+
+BASEDIR=$(dirname $0)
+
+echo "=> Connect to '$SERVER' server and install pushand:"
+ssh $SERVER <<EOF
+pushd \$(mktemp -d /tmp/pushand.install.XXXXXXXXXX)
+  git clone git://github.com/fesplugas/pushand.git
+  sudo ./pushand/uninstall.sh
+  sudo ./pushand/install.sh
+popd
+EOF
+
+echo "=> Create a local repo and push it to the server:"
+pushd $(mktemp -d /tmp/pushand.XXXXXXXXXX)
+  git init
+
+  echo "=> Create a '.pushand' file and make it executable:"
+  cat > \.pushand << EOF
+#!/bin/bash
+echo "Hello from .pushand ... this file is \$0"
+EOF
+  chmod +x \.pushand
+
+  echo "=> Add file to git and commit it:"
+  git add .
+  git commit -m 'First Commit'
+
+  echo "=> Add a remote to git:"
+  git remote add pushand $SERVER:$(pwd)
+
+  echo "=> Verify git config:"
+  cat .git/config
+
+  echo "=> Push the repo:"
+  git push pushand master
+popd
